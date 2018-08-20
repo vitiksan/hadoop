6f8f316e408dc39a87c0dfe6c38641c05c877196
Vitalii Makh <vmakh@softserveinc.com>
2018-08-20 19:06:24 +0300

    fix AWS sns and clean code

--- src/main/java/com/learning/AwsSnsMessage.java
+++ src/main/java/com/learning/AwsSnsMessage.java
@@ -1,39 +1,22 @@
 package com.learning;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sns.AmazonSNSClient;
-import com.amazonaws.services.sns.model.CreateTopicResult;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class AwsSnsMessage {
     private AmazonSNSClient snsClient;
-    private String topicArn;
 
     public AwsSnsMessage() {
-        AWSCredentialsProvider awsCredentialsProvider = new ClasspathPropertiesFileCredentialsProvider();
-        this.snsClient = new AmazonSNSClient(new ClasspathPropertiesFileCredentialsProvider());
-        this.snsClient.setRegion(Region.getRegion(Regions.US_EAST_2));
+        this.snsClient = new AmazonSNSClient();
     }
 
-    public void createTopic() {
-        CreateTopicResult result = snsClient.createTopic("JobStatus");
-        topicArn =  result.getTopicArn();
-    }
-
-    public void subscribe(String email){
-        snsClient.subscribe(topicArn, "email", email);
-
-    }
-
-    public void sendMessage(String message) {
-        snsClient.publish(topicArn, message);
-    }
-
-    public static void main(String[] args) {
-        AwsSnsMessage snsMessage = new AwsSnsMessage();
-        snsMessage.createTopic();
-        snsMessage.subscribe("patriot02faqq@gmail.com");
+    public void sendMessage(String message) throws IOException {
+        InputStream inputStream = WordCount.class.getClassLoader().getResourceAsStream("./AwsCredentials.properties");
+        Properties aws = new Properties();
+        aws.load(inputStream);
+        snsClient.publish(aws.getProperty("topicArn"), message);
     }
 }
--- src/main/java/com/learning/WordCount.java
+++ src/main/java/com/learning/WordCount.java
@@ -12,7 +12,6 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-import java.util.Scanner;
 
 public class WordCount {
 
@@ -30,12 +29,13 @@ public class WordCount {
         job.setOutputFormatClass(TextOutputFormat.class);
 
         if (onS3) {
-            InputStream inputStream = WordCount.class.getClassLoader().getResourceAsStream("AwsCredentials.properties");
+            InputStream inputStream = WordCount.class.getClassLoader().getResourceAsStream("./AwsCredentials.properties");
             Properties aws = new Properties();
             aws.load(inputStream);
             job.getConfiguration().set("fs.s3n.awsAccessKeyId", aws.getProperty("accessKey"));
             job.getConfiguration().set("fs.s3n.awsSecretAccessKey", aws.getProperty("secretKey"));
             job.getConfiguration().set("fs.defaultFS", "s3n://" + aws.getProperty("bucketName"));
+
         }
 
         FileInputFormat.addInputPath(job, new Path(input));
@@ -46,46 +46,24 @@ public class WordCount {
 
     public static void main(String[] args) throws Exception {
         boolean onS3 = args[0].equals("s3");
-        String inputPath;
-        String outputPath;
-        String email;
+        String inputPath, outputPath;
 
-        if (onS3 && args.length == 4) {
+        if (onS3 && args.length == 3) {
             inputPath = args[1];
             outputPath = args[2];
-            email = args[3];
-        } else if (!onS3 && args.length == 3) {
+        } else if (!onS3 && args.length == 2) {
             inputPath = args[0];
             outputPath = args[1];
-            email = args[2];
         } else {
             System.out.println("Error! False list of arguments!");
             return;
         }
 
-        AwsSnsMessage snsMessage = new AwsSnsMessage();
-        snsMessage.createTopic();
-        snsMessage.subscribe(email);
-        System.out.println("Email for subscribe on this job has sent.");
-
-        String temp;
-        do {
-            System.out.println("Confirm it and print : Yes (y) or Not (n) if you did not receive email");
-            Scanner in = new Scanner(System.in);
-            temp = in.next();
-            if (temp.equalsIgnoreCase("n")) {
-                System.out.print("Re-enter email: ");
-                email = in.next();
-                snsMessage.subscribe(email);
-                System.out.println("Message has sent again");
-            }
-        } while (!temp.equalsIgnoreCase("y"));
-
-
         Job job = setUp(inputPath, outputPath, onS3);
 
+        boolean isSuccessfully = job.waitForCompletion(true);
+        AwsSnsMessage snsMessage = new AwsSnsMessage();
 
-        boolean isSuccessfully = job.waitForCompletion(false);
         if (isSuccessfully)
             snsMessage.sendMessage("Job finished successfully");
         else
